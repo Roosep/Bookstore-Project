a49fe9e9e29f02cabfa938309399c071e0bbdb5b
Seppis <roopes16@hotmail.com>
2022-09-07 10:20:15 +0300

    First commit

---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,13 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private long price;
+	
+	
+
+}
