5c303c29fffa0b5f53bcd81001ace2050980d686
Roosep <roopes16@hotmail.com>
2022-10-25 18:53:37 +0300

    Second Commit

---
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -0,0 +1,31 @@
+package com.example.Bookstore;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			Book s1 = new Book("And Then There Were None", "Agatha Christie", 1939, "978-0062073488", 20.00);
+			Book s2 = new Book("To Kill A Mockingbird", "Harper Lee", 1988, "978-0446310789", 15.99);
+			Book s3 = new Book("The Boy in the Striped Pajamas", "John Boyne", 2006, "978-0385751063", 17.99);
+			
+			repository.save(s1);
+			repository.save(s2);
+			repository.save(s3);
+		};
+	}
+}
---
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,67 @@
+package com.example.Bookstore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String title;
+	private String author;
+	private int publishYear;
+	private String isbn;
+	private double price;
+	
+	public Book() {}
+	
+	public Book(String title, String author, int publishYear, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.publishYear = publishYear;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getpublishYear() {
+		return publishYear;
+	}
+	public void setpublishYear(int publishYear) {
+		this.publishYear = publishYear;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publishYear=" + publishYear + ", isbn=" + isbn
+				+ ", price=" + price + "]";
+	}
+}
